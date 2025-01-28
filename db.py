f2d3f8f3a44dd06c31dc7de5151011b2ee881cb0
Ben Roberts <ben@benroberts.me>
2025-01-28 14:07:02 -0800

    Version/compat updates

--- db.py
+++ db.py
@@ -3,13 +3,13 @@ import urllib.parse
 import configparser
 
 # Read the config file and generate a database connection string
-def build_engine_string():
+def build_engine_string(configpath='db.ini'):
     config = configparser.ConfigParser()
-    config.read('db.ini')
+    config.read(configpath)
     dbconfig = dict(config.items('db'))
     dbconfig['password'] = urllib.parse.quote_plus(dbconfig['password'])
     return "postgresql://{username}:{password}@{hostname}:{port}/{dbname}".format(**dbconfig)
 
-def connect():
-    return create_engine(build_engine_string())
+def connect(config='db.ini'):
+    return create_engine(build_engine_string(config))
 
